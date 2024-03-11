e0b4b7648393e9d87320afcd2bfee4967811de25
thaiduong1511 <thaiduong1511@gmail.com>
2024-03-11 22:24:49 +0700

    them mqtt

---
+++ mqtt/Publish.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MQTTnet;
+using MQTTnet.Client;
+
+Console.WriteLine("-- MQTT Publisher --");
+var mqttFactory = new MqttFactory();
+using var mqttClient = mqttFactory.CreateMqttClient();
+var mqttClientOptions = new MqttClientOptionsBuilder()
+    .WithClientId(Guid.NewGuid().ToString())
+    .WithTcpServer("192.168.1.7",1883)
+    .WithCredentials("homeassistant", "thaphie2ipoo0fooghooch7ahLaecoo2aes8reeleetheengoul5ahraen7rootu")
+    .WithCleanSession()
+    .Build();
+
+await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+if (mqttClient.IsConnected)
+{
+    while (true) {
+    Console.Write("Message >>> ");
+    var payload = Console.ReadLine();
+    if (payload == "quit") break;
+
+    var applicationMessage = new MqttApplicationMessageBuilder()
+    .WithTopic("homeassistant/switch/1/power")
+    .WithPayload(payload)
+    .Build();
+
+    await mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+    }
+
+    await mqttClient.DisconnectAsync();
+}
+else 
+{
+    Console.WriteLine("Can not connect");
+}
+
---
+++ mqtt/Subcibe.cs
@@ -0,0 +1,35 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System.Text;
+
+
+Console.WriteLine("-- MQTT Subscriber --");
+var mqttFactory = new MqttFactory();
+using var mqttClient = mqttFactory.CreateMqttClient();
+var mqttClientOptions = new MqttClientOptionsBuilder()
+                        .WithClientId(Guid.NewGuid().ToString())
+                        .WithTcpServer("192.168.1.7", 1883)
+                        .WithCredentials("homeassistant", "thaphie2ipoo0fooghooch7ahLaecoo2aes8reeleetheengoul5ahraen7rootu")
+                        .WithCleanSession()
+                        .Build();
+
+mqttClient.ApplicationMessageReceivedAsync += OnMessageReceived;
+
+await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+    .WithTopicFilter(
+        f => {
+            f.WithTopic("homeassistant/#");
+        })
+    .Build();
+
+await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+
+Console.WriteLine("MQTT client subscribed to topic.");
+Thread.Sleep(-1);
+
+static async Task OnMessageReceived(MqttApplicationMessageReceivedEventArgs e) {
+Console.WriteLine($"# Received message: {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+await Task.CompletedTask;
+}
